424a76b0b327a0bc0540534f9925a93884ee0d6b
zry754331875 <754331875@qq.com>
2018-07-18 11:19:39 +0800

    1

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -21,7 +21,7 @@ class Login extends PureComponent {
         const {isLoading,isSuccess,loginError} = this.props
 
         if(isLoading){
-          message.loading('正在登陆')
+          message.loading('正在登陆中')
         }
         if(loginError != null){
           const {message:errorMsg} = loginError
